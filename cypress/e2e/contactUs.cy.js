e185ce9d38e0c048143ec7d264729694ca4048aa
j-Abban <justice.abban@amalitech.org>
2024-06-05 16:03:44 +0000

    I have automated the CONTACT US functionality

---
+++ cypress/e2e/contactUs.cy.js
@@ -0,0 +1,32 @@
+describe('Navigation to Contact Us Page Test', () => {
+    beforeEach(() => {
+    // Visit the website
+      cy.visit('http://webdriveruniversity.com/');
+  
+    });
+    it('Should navigate to Contact Us page and verify URL', () => {
+      // Click on the Contact Us link
+      cy.get('#contact-us').invoke('removeAttr', 'target').click();
+  
+      // Wait for the new window to open and switch to it
+      cy.window().then(win => {
+        cy.stub(win, 'open').as('windowOpen');
+      });
+  
+      // Assertion to verify the URL of the new window
+      cy.url().should('include', '/Contact-Us/contactus.html');
+      cy.get('.section_header').contains('CONTACT US');
+
+      // Enter your first name, last name, email address and comments
+      cy.get('input[name="first_name"]').type('Daniel Panyin');
+      cy.get('input[name="last_name"]').type('Mensah');
+      cy.get('input[name="email"]').type('dannymens@gmail.com');
+      cy.get('textarea.feedback-input').type('I just want to say a very big thank you to the developers who created this piece. Cheers!');
+    
+      // Click on the Submit button
+      cy.get('input[type="submit"]').click();
+      cy.url().should('include', '/contact-form-thank-you.html');
+      cy.get('div[id="contact_reply"]').contains('Thank You for your Message!');
+    });
+  });
+  
