cfa88d9428e77599049cfc50a6195b1ecffa94d2
j-Abban <justice.abban@amalitech.org>
2024-06-08 18:53:24 +0000

    Data, Tables & Button States

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -242,5 +242,21 @@ describe('WebDriverUniversity', () => {
       cy.url().should('include', 'http://webdriveruniversity.com/');
       cy.wait(2000);
      });
+
+     it('should navigate to Data, Tables & Button States', () => {
+      // Click on the DATA, TABLES & BUTTON States
+      cy.get('#data-table').invoke('removeAttr', 'target').click();
+      // Wait for new window to open and switch to it
+      cy.window().then(win => {
+       cy.stub(win, 'open').as('windowOpen');
+      });
+      // Assert the url and other element on the page
+      cy.url().should('include','/Data-Table/index.html');
+      cy.get('#nav-title').should('have.text','WebdriverUniversity.com (Data Tables)');
+      cy.get('#main-header').should('include.text','Data, Tables & Button States');
+      // Navigates back to the home page
+      cy.go(-1);
+      cy.url().should('include', 'http://webdriveruniversity.com/');
+     });
   });
   
