bc8ac63a81d755c4da370ad2b521c16b77f883e4
j-Abban <justice.abban@amalitech.org>
2024-06-11 11:31:41 +0000

    JavaScript Click

--- cypress/e2e/buttonClicks.cy.js
+++ cypress/e2e/buttonClicks.cy.js
@@ -11,38 +11,59 @@ before(() => {
         // Allow othe errors to fail the test
         return true;
         });
-
-    // Visit the website url (find the baseUrl in cypress.config file)
-    cy.visit('/');
-    //Assert that the url is correct
-    cy.url().should('eq', 'http://webdriveruniversity.com/');
 });
 
-it('should Click on WebElement Click', () =>{
-    // click on Button Clicks link
+beforeEach(() => {
+     // Visit the website url (find the baseUrl in cypress.config file)
+     cy.visit('/');
+     //Assert that the url is correct
+     cy.url().should('eq', 'http://webdriveruniversity.com/');
+
+      // click on Button Clicks link
     cy.get('#button-clicks').invoke('removeAttr', 'target').click();
+
     // Wait for the new window to open and switch to it
     cy.window().then(win => {
     cy.stub(win, 'open').as('windowOpen');
     });
+
     // Verify that the url is working
     cy.url().should('include', '/Click-Buttons/index.html');
-
-   // Assert that there is "Lets Get Clicking!" text on the page
+    // Assert that there is "Lets Get Clicking!" text on the page
   cy.get('#main-header').should('include.text', 'Lets Get Clicking!');
-  // Assert that there is "Lets Get Clicking!" text on the page
+});
+
+it('should Click on WebElement Click', () =>{
+  // Assert that there is "WebElement Click" text on the page
   cy.get('div:nth-of-type(1) > .thumbnail > h2').should('have.text', 'WebElement Click');
   // Click on the "CLICK ME!" button
   // Click on the "CLICK ME!" button
   cy.get('#button1 > p').click();
- cy.wait(2000);
+  cy.wait(2000);
+
   // Verify the modal content and close the modal
   cy.get('#myModalClick > .modal-dialog > .modal-content > .modal-header > .modal-title').should('include.text', 'Congratulations!');
-  cy.get('.modal-body').should('include.text', 'Well done for successfully using the click() method!');
+  cy.get('div#myModalClick > .modal-dialog.modal-sm p').should('include.text', 'Well done for successfully using the click() method!');
 
   // Close the alert by clicking "Close"
   cy.on('window:confirm', () => true);
   cy.get('div#myModalClick > .modal-dialog.modal-sm .btn.btn-default').click();
+});
+
+
+it('should click on JavaScipt Click', () =>{
+    // Assert that there is "JavaScript Click" text on the page
+  cy.get('div:nth-of-type(2) > .thumbnail > h2').should('have.text', 'JavaScript Click');
+  // Click on the "CLICK ME!" button
+  cy.get('div:nth-of-type(2) > .thumbnail > .caption > .btn.btn-default.btn-lg.dropdown-toggle').click();
+  cy.wait(2000);
+  // Verify the modal content and close the modal
+  cy.get('.modal-dialog.modal-md .modal-title').should('include.text', 'It’s that Easy!!  Well I think it is.....');
+  cy.get('.modal-dialog.modal-md p').should('include.text', 'We can use JavaScript code if all else fails! Remember always try to use the WebDriver Library method(s) first such as WebElement.');
+
+  // Close the alert by clicking "Close"
+  cy.on('window:confirm', () => true);
+  cy.get('.modal-dialog.modal-md .btn.btn-default').click();
   cy.wait(2000);
 });
 });
